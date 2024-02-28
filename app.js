7a85c4926f02c8e2011ff980afc88fba61fc2dd4
Dineshbhandari2023 <iamdinesh.github@gmail.com>
2024-02-28 23:30:43 +0545

    Basics practice of node

---
+++ app.js
@@ -0,0 +1,96 @@
+
+// const os = require('os');
+// var totalMemory = os.totalmem();
+// var freeMemory = os.freemem();
+// console.log(`Total Memory: ${totalMemory}`);
+// console.log(`Free Memory: ${freeMemory}`);
+
+
+
+// const fs = require('fs');
+// const files = fs.readdirSync('./');
+// console.log(files);
+// fs.readdir('$', function (err, files) {
+//     if (err) console.log(`Error: ${err}`);
+//     else console.log(`Result: ${files}`)
+// });
+
+
+
+// const EventEmitter = require('events');
+// // const emitter = new EventEmitter();
+
+// const Logger = require('./logger');
+// const logger = new Logger();
+
+// // register a listener
+// logger.on('messaged logged!', (arg) => {
+//     console.log('listener called!', arg);
+// });
+
+// logger.log('message'); 
+
+
+
+
+// creating server object:
+// http.server(function (req, res) {
+//     res.write('Hello Dinesh!');
+//     res.end();
+// }).listen(8080);
+
+
+// const http = require('http');
+// const server = http.createServer((req, res) => {
+//     if (req.url === '/') {
+//         res.write('Hellow Dinesh!!');
+//         res.end();
+//     }
+//     if (req.url === '/dinesh/courses') {
+//         res.write(JSON.stringify([1, 2, 2, 3, 4]));
+//         res.end();
+//     }
+// });
+// server.on('conection', (socket) => {
+//     console.log('New connection!!!!!!!!');
+// });
+// server.listen(3000);
+// console.log('Listening on port 3000.....');
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ logger.js
@@ -0,0 +1,16 @@
+// const EventEmitter = require('events');
+// // const emitter = new EventEmitter();
+
+// var url = 'http://dinesh.io/log';
+
+// class Logger extends EventEmitter {
+//     log(message) {
+//         //send an http request
+//         console.log(message);
+    
+//         // Raising event
+//         this.emit('messaged logged!', { id: 1, url: 'dinesh.com'});
+//     }    
+// }
+
+// module.exports = Logger;
